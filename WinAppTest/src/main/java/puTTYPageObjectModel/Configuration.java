88c8ae9a46e7e81870f6379f7bdd58d7b2056979
mytyas <143218208+mytyas@users.noreply.github.com>
2023-09-08 13:27:24 +0200

    Update Configuration.java

--- WinAppTest/src/main/java/puTTYPageObjectModel/Configuration.java
+++ WinAppTest/src/main/java/puTTYPageObjectModel/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration extends Functions {
     }
 
     public Console setUpPutty(){
-        driver.findElementByClassName("Edit").findElement(By.name("Host Name (or IP address)")).sendKeys("10.42.2.51");
+        driver.findElementByClassName("Edit").findElement(By.name("Host Name (or IP address)")).sendKeys("ipaddress");
         driver.findElementByName("Logging").click();
         driver.findElementByName("Printable output").click();
         driver.findElementByAccessibilityId("1050").clear();
